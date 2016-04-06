c3b7899d9bea7e5b9733590fb8a426f0d92da40c
wietze <wietze@vdmi.nl>
2016-04-06 22:45:45 +0200

    Settings and login page

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://dev.geld.wiet';
 
 /*
 |--------------------------------------------------------------------------
@@ -261,7 +261,7 @@ $config['log_file_permissions'] = 0644;
 | codes to set your own date formatting
 |
 */
-$config['log_date_format'] = 'Y-m-d H:i:s';
+$config['log_date_format'] = 'd-m-Y H:i:s';
 
 /*
 |--------------------------------------------------------------------------
